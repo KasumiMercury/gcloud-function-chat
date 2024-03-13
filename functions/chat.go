766c5dfa5776ab512a2f5748aba27981a3e1f833
KasumiMercury <naoki_shira@outlook.jp>
2024-03-14 02:18:50 +0900

    :construction: Create YouTube service / for fetching chat

--- functions/chat.go
+++ functions/chat.go
@@ -2,9 +2,12 @@ package functions
 
 import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 	"log/slog"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 )
 
@@ -22,8 +25,24 @@ func init() {
 }
 
 func chatWatcher(w http.ResponseWriter, r *http.Request) {
+	// Cache environment variables
+	// Because the function is supposed to run on CloudFunctions, it is necessary to read the environment variables here.
+	ytApiKey := os.Getenv("YOUTUBE_API_KEY")
+	if ytApiKey == "" {
+		slog.Error("YOUTUBE_API_KEY is not set")
+		w.WriteHeader(http.StatusInternalServerError)
+		panic("YOUTUBE_API_KEY is not set")
+	}
+
 	// Initialize span
 	span := getSpanQuery(r.URL)
+
+	// Create YouTube service
+	ytSvc, err := youtube.NewService(r.Context(), option.WithAPIKey(ytApiKey))
+	if err != nil {
+		slog.Error("Failed to create YouTube service: %v", err)
+		return
+	}
 	slog.Info("chatWatcher")
 }
 
