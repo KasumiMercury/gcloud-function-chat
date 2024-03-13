2b2aecb01f135fb70ffd1f7e4ed7ee107f8dffff
KasumiMercury <naoki_shira@outlook.jp>
2024-03-13 20:40:37 +0900

    :construction: Extract query / to support both scheduled acquisitions and manual custom acquisitions

--- functions/chat.go
+++ functions/chat.go
@@ -4,6 +4,8 @@ import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"log/slog"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 func init() {
@@ -20,5 +22,36 @@ func init() {
 }
 
 func chatWatcher(w http.ResponseWriter, r *http.Request) {
+	// Initialize span
+	span := getSpanQuery(r.URL)
 	slog.Info("chatWatcher")
+}
+
+func getSpanQuery(u *url.URL) int {
+	group := slog.Group("getSpanQuery")
+	// Default value is 60 minutes
+	// Because the update timing of the service group in the upper layer is every 60 minutes
+	defVal := 60
+
+	// Check if the request has a query parameter named "span"
+	// If it does, return the value of the parameter as an integer
+	// If it does not, return default value
+	// If the value is not a number, return default value
+
+	// Get the value of the query parameter named "span"
+	span := u.Query().Get("span")
+	if span == "" {
+		slog.Info("span is empty", group)
+		return defVal
+	}
+
+	// Convert the value to an integer
+	spanInt, err := strconv.Atoi(span)
+	if err != nil {
+		slog.Error("Failed to set span because of invalid value", group)
+		return defVal
+	}
+
+	// Return the value
+	return spanInt
 }
