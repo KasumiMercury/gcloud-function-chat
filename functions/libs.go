fdfcad0a7af68a2a688a37e9083955a0b8c4a043
KasumiMercury <naoki_shira@outlook.jp>
2024-03-15 01:20:26 +0900

    :construction: Fix treat of log group (initialize)

--- functions/libs.go
+++ functions/libs.go
@@ -9,7 +9,6 @@ import (
 )
 
 func getSpanQuery(u *url.URL) int {
-	group := slog.Group("getSpanQuery")
 	// Default value is 60 minutes
 	// Because the update timing of the service group in the upper layer is every 60 minutes
 	defVal := 60
@@ -22,14 +21,14 @@ func getSpanQuery(u *url.URL) int {
 	// Get the value of the query parameter named "span"
 	span := u.Query().Get("span")
 	if span == "" {
-		slog.Info("span is empty", group)
+		slog.Info("span is empty")
 		return defVal
 	}
 
 	// Convert the value to an integer
 	spanInt, err := strconv.Atoi(span)
 	if err != nil {
-		slog.Error("Failed to set span because of invalid value", group)
+		slog.Error("Failed to set span because of invalid value")
 		return defVal
 	}
 
